9d2bf9c803c7a0bc8c25da2ea7ca13ac7b126d95
ark10806 <ark10806@gmail.com>
2021-05-04 00:03:14 +0900

    more faster

--- mental_basasak.py
+++ mental_basasak.py
@@ -6,9 +6,11 @@ sp = ' '
 strs = '정신나갈것같아'
 
 while True:
+    print('\t'*randint(0,5), end='')
+    sys.stdout.flush()
     for s in strs:
         print(f'{sp * randint(0,7)}{s}', end='')
-        time.sleep(0.05)
+        time.sleep(0.02)
         sys.stdout.flush()
     print('\n'*randint(0,3), end='')
     sys.stdout.flush()
